a8ab68204f1a45538e93abdd6d9a624463f77bfc
Berns <bernie.remollo@yahoo.com>
2017-09-16 21:02:05 +0800

    Added array in array

--- model/scope/populate_table_main.php
+++ model/scope/populate_table_main.php
@@ -7,12 +7,16 @@
   $q = $conn->prepare($sql);
   $q -> execute();
   $browse = $q -> fetchAll();
+  $test = [['project_name'=> 'project1', 'project_no'=> '1'],['project_name'=> 'project2', 'project_no'=>'2']];
   foreach($browse as $fetch)
   {
   $output[] = array(
-      'scope_id'=> $fetch['scope_id'], 'scope_name'=> $fetch['scope_name']
+      'scope_id'=> $fetch['scope_id'],
+      'scope_name'=> $fetch['scope_name'],
+      'scope_project'=> $test
   );
   }
+  
   $conn = null;
 
 echo json_encode($output);
