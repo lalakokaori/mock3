74c3bcb589579e1db604d47b6bb42909c35b05b6
Berns <bernie.remollo@yahoo.com>
2017-09-16 20:37:20 +0800

    Shit nasira

--- model/scope/populate_table_main.php
+++ model/scope/populate_table_main.php
@@ -1,13 +1,3 @@
-<?php
-    include('../master/connect.php');
-
-
-  //$sql = "SELECT labor_id,category_name,labor_name,unit_name,rate FROM labor as l,unit as u,categories as c where l.status = 'active' and l.category_id=c.category_id and l.unit_id=u.unit_id ORDER BY labor_name desc";
-    $sql = "SELECT * FROM scope where status='active'";
-  $q = $conn->prepare($sql);
-  $q -> execute();
-  $browse = $q -> fetchAll();
-  foreach($browse as $fetch)
   {
   $output[] = array(
       'scope_id'=> $fetch['scope_id'], 'scope_name'=> $fetch['scope_name']
@@ -18,4 +8,18 @@
 echo json_encode($output);
 ?>
 
+/* DEVELOP BRANCH B*/
+/* DEVELOP BRANCH B*/
+/* DEVELOP BRANCH B*/
+/* DEVELOP BRANCH B*/
+/* DEVELOP BRANCH B*/
+/* DEVELOP BRANCH B*/
+/* DEVELOP BRANCH B*/
+/* DEVELOP BRANCH B*/
+/* DEVELOP BRANCH B*/
+/* DEVELOP BRANCH B*/
+/* DEVELOP BRANCH B*/
+/* DEVELOP BRANCH B*/
+/* DEVELOP BRANCH B*/
+/* DEVELOP BRANCH B*/
 /* DEVELOP BRANCH B*/
